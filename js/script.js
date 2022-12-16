70c6b27c557c50a19c9bea43c17a6a5b2ac32dff
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-12-16 19:27:06 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+// seleção de elementos
+const todoForm = document.querySelector('#todo-form')
+const todoInput = document.querySelector('#todo-input')
+const todoList = document.querySelector('#todo-list')
+const editform = document.querySelector('#edit-form')
+const editForm = document.querySelector('#edit-input')
+const cancelEditBtn = document.querySelector('#cancel-edit-btn')
+
+
+// funções
+todoForm.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+
+    const inputValue = todoInput.value
+
+    if (inputValue) {
+        
+    }
+})
+
+
+
+// eventos
