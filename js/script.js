a861d85965fd81fd6f5db76d6654a3f4f699fd74
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2022-12-16 23:23:49 -0300

    att

--- js/script.js
+++ js/script.js
@@ -7,8 +7,49 @@ const editform = document.querySelector('#edit-form')
 const editForm = document.querySelector('#edit-input')
 const cancelEditBtn = document.querySelector('#cancel-edit-btn')
 
+let oldInputValue
+
 
 // funções
+const saveTodo = (text) =>{
+    const todo = document.createElement('div')
+    todo.classList.add('todo')
+
+    const todoTitle = document.createElement('h3')
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement('button')
+    doneBtn.classList.add('finish-todo')
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit-todo')
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('remove-todo')
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+
+    todoInput.value = ''
+    todoInput.focus()
+}
+
+const toggleForm = () =>{
+    editForm.classList.toggle('hide')
+    todoForm.classList.toggle('hide')
+    todoList.classList.toggle('hide')
+}
+
+
+
+// eventos
+
 todoForm.addEventListener('submit', (e) => {
 
     e.preventDefault()
@@ -16,10 +57,37 @@ todoForm.addEventListener('submit', (e) => {
     const inputValue = todoInput.value
 
     if (inputValue) {
-        
+        saveTodo(inputValue)
     }
 })
 
+document.addEventListener('click', (e) =>{
+    const targetEl = e.target
+    const parentEl = targetEl.closest('div')
+    let todoTitle
 
+    if (parentEl && parentEl.querySelector('h3')) {
+        todoTitle = parentEl.querySelector('h3').innerText
+    }
 
-// eventos
+    if (targetEl.classList.contains('finish-todo')) {
+        parentEl.classList.toggle('done')
+    }
+
+    if (targetEl.classList.contains('remove-todo')) {
+        parentEl.remove()
+    }
+
+    if (targetEl.classList.contains('edit-todo')) {
+        toggleForm()
+
+        editInput.value = todoTitle
+        oldInputValue.value = todoTitle
+    }
+})
+
+cancelEditBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    toggleForm()
+})
